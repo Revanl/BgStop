16f42702c59892d81c7fecc51fc3f2db4789435a
Iani Garfalski <ianihrg@abv.bg>
2018-01-05 14:54:53 +0200

    Update app.blade.php

--- bg_stop1/resources/views/layout/app.blade.php
+++ bg_stop1/resources/views/layout/app.blade.php
@@ -1,145 +1,221 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-       <!-- <title>App Name - yield('title')</title> -->
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<head>
+    <!-- <title>App Name - yield('title')</title> -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- jQuery library -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <meta property="fb:app_id"          content="1234567890" />
+    <meta property="og:type"            content="article" />
+    <meta property="og:url"             content="https://bgstop.co.uk" />
+    <meta property="og:title"           content="BGSTOP" />
+    <meta property="og:image"           content="https://bgstop.co.uk/img/bg_stop_logo_black.png" />
+    <meta property="og:description"    content="Мултифункционален уеб сайт
+в който хората живеещи в Обединеното
+кралство могат да намерят работа, да продадат,
+да наемат, да вземат уроци, да се
+запознават, да общуват на форум и създавайки
+усещането, че сте в едно българско общество, което е далеч от родината." />
 
-        <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        @auth
-            <script>
 
-                var user = '{!! json_encode( Auth()->user()->id ) !!}';
+    <title>bgstop</title>
+    <meta name="description" content="Мултифункционален уеб сайт
+в който хората живеещи в Обединеното
+кралство могат да намерят работа, да продадат,
+да наемат, да вземат уроци, да се
+запознават, да общуват на форум и създавайки
+усещането, че сте в едно българско общество, което е далеч от родината.">
+    <meta name="keywords" content="uk, bg, immigrants, work, purchase, rent, housing, forum, bgforum, bgukforum, dating, english, bulgarian, community, bgstop, lessons, services">
 
-            </script>
-         @endauth
-    </head>
 
-    <body class="text-center">
-        <div id="wrapper" class="text-center">
-            <header>
-                <div class="col-sm-4">
-                    <div class="logoHookParent">
-                        <a href="/">
-                        <img src="/img/bg_stop_logo.png" alt="bg_stop">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-sm-2">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-                    {{--<div class="col-xs-6">--}}
-                        {{--времето тук--}}
-                    {{--</div>--}}
-                    {{--<div class="col-xs-6">--}}
-                        {{--валутата тук--}}
-                    {{--</div>--}}
-                </div>
-                <div class="col-sm-6">
-                    @include('messages.messages')
-                    @auth
-                        @include('messages.contactsManager')
-                        @include('messages.chat')
-                    @endauth
-                    <div class="flex-center position-ref full-height hideOnMobile">
-                        @if (Route::has('login'))
-                            <div class="top-right links">
-                                @auth
-                                    <a href="{{ url('/') }}">Начало</a>
-                                    <a href="{{url('/edit')}}">Редакция</a>
-                                    <a href="{{url('logout')}}">Изход</a>
-                                @else
-                                    <a href="{{ route('login') }}">Вход</a>
-                                    <a href="{{ route('register') }}">Регистрация</a>
-                                @endauth
-                            </div>
-                        @endif
-                    </div>
-                </div>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-                    <img class="mainImg" src="/img/uk.jpg"/>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/css/style.css">
+    @auth
+    <script>
 
-                {{--<a href=" http://www.commercialclean4you.co.uk">--}}
-                    {{--<img class="mainImg" src="/img/cleaning.png" alt="bg_stop_Zimg"/>--}}
-                {{--</a>--}}
-                @include('inc.nav')
-                <img class="dropDownButton" src="/img/navbar-slide.png"/>
-            </header>
+        var user = '{!! json_encode( Auth()->user()->id ) !!}';
+
+    </script>
+    @endauth
+    <style>
+        /*body { white-space: nowrap; }*/
+        /*div{display:inline-block}*/
+    </style>
+</head>
+{{ config('app.title') }}
+<body class="text-center" style="width:1200px;white-space: nowrap;margin:auto;">
+<div id="wrapper" class="text-center"   >
+    <header>
+        <div class="col-xs-4">
+
+            @auth
+            @include('messages.contactsManager')
+            @include('messages.chat')
+            @endauth
+
             <div class="col-xs-12">
-                <div class="col-xs-2">
-                    <img src="/img/adone.png" style="width:200px;height:220px">
-                    <br>
-                    <br>
-                    <img src="/img/adtwo.png" style="width:200px;height:200px">
-                </div>
-                <div class="container col-xs-8" style="float:right;">
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-<!-- Indicators -->
-<ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-
-</ol>
-
-<!-- Wrapper for slides -->
-<div class="carousel-inner">
-    <div class="item active">
-        <img src="/img/webandapp.png" style="width:100%;height:300px!important">
-    </div>
-    <div class="item">
-        <img src="/img/cleaning.png" style="width:100%;height:300px!important">
-    </div>
-</div>
-
-<!-- Left and right controls -->
-<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-</a>
-<a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-</a>
-</div>
-                    @include('inc.messages')
-                    @yield('content')
-                    <img src="/img/webandapp.png" style="width:100%;height:300px">
-                </div>
-                <div class="col-xs-2"  style="height:300px">
-                   <img src="/img/builders.png" style="width:100%;height:300px">
-                    <br>
-                    <br>
-                   <img src="/img/electrician.png" style="width:100%;height:300px">
-                </div>
+                <img src="/img/bgflag.jpg" height="190" width="190" alt="bg_stop_Zimg"/>
             </div>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="{{ asset('js/custom.js') }}"></script>
-        <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-        {{--<script src="/vendor/unisharp/tinymce/js/tinymce/tinymce.min.js"></script>--}}
-        {{--<script>tinymce.init({ selector:'textarea' });</script>--}}
-        <script>
+        <div class="col-xs-4">
 
-            CKEDITOR.replace( 'article-ckeditor'  );
-        </script>
-    </body>
-    <footer>
-        {{--<div class="col-xs-6"  style="height:300px">--}}
-            {{--<img src="/img/repair.png" style="width:100%;height:400px">--}}
-        {{--</div>--}}
+            <div class="logoHookParent">
+                <a href="/">
+                    <img src="/img/bg_stop_logo_black.png" alt="bg_stop">
+                </a>
+            </div>
+            {{--<div class="col-xs-6">--}}
+            {{--времето тук--}}
+            {{--</div>--}}
+            {{--<div class="col-xs-6">--}}
+            {{--валутата тук--}}
+            {{--</div>--}}
+        </div>
+        <div class="col-sm-4 mailbox">
+            @include('messages.messages')
+            <div class="flex-center position-ref full-height hideOnMobile">
+                @if (Route::has('login'))
+                    <div class="top-right links">
+                        @auth
+                        <a href="{{ url('/') }}">Начало</a>
+                        <a href="{{url('/edit')}}">Редакция</a>
+                        <a href="{{url('logout')}}">Изход</a>
+                        @else
+                            <a href="{{ route('login') }}">Вход</a>
+                            <a href="{{ route('register') }}">Регистрация</a>
+                            @endauth
+                    </div>
+                @endif
+            </div>
 
-        {{--<div class="col-xs-6"  style="height:300px">--}}
-            {{--<img src="/img/taxilandscape.jpg" style="width:100%;height:400px">--}}
+        </div>
+
+        <img class="mainImg" src="/img/uk.jpg"/>
+
+
+
+    @include('inc.nav')
+    <!--<img class="dropDownButton" src="/img/stop.png"/>-->
+    </header>
+    <div style="width:100%;float:left;clear:both;">
+        <div style="width:200px;display:inline-block;float:left">
+            <img src="/img/repair.png" style="width:200px;height:200px">
+            <br>
+            <br>
+            <img src="/img/taxi.png" style="width:200px;height:200px">
+        </div>
+        <div style="width:200px;float:right;">
+            <img src="/img/builders.png" style="width:300px;height:300px">
+            <br>
+            <br>
+            <img src="/img/electrician.png" style="width:300px;height:300px">
+        </div>
+        <div class="container" style="width:800px;display:inline-block;overflow:visible;white-space:normal">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="/img/webandapp.png" style="width:100%;height:300px!important">
+                    </div>
+                    <div class="item">
+                        <img src="/img/advenci.png" style="width:100%;height:300px!important">
+                    </div>
+                    <div class="item">
+                        <a href="http://www.commercialclean4you.co.uk/"><img src="/img/cleaning.png" style="width:100%;height:300px!important;"></a>
+                    </div>
+                    <div class="item">
+                        <img src="/img/addani.png" style="width:100%;height:300px!important">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        @include('inc.messages')
+        @yield('content')
+        <!--                <div class="container" style="width:800px;display:inline-block;overflow:visible;white-space:normal">-->
+            <!--<div id="myCarousel" class="carousel slide" data-ride="carousel">-->
+            <!-- Indicators -->
+            <!--<ol class="carousel-indicators">-->
+            <!--    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>-->
+            <!--    <li data-target="#myCarousel" data-slide-to="1"></li>-->
+            <!--    <li data-target="#myCarousel" data-slide-to="2"></li>-->
+            <!--</ol>-->
+
+            <!-- Wrapper for slides -->
+            <!--<div class="carousel-inner">-->
+            <!--    <div class="item active">-->
+            <!--        <img src="/img/webandapp_bg.png" style="width:100%;height:300px!important">-->
+            <!--    </div>-->
+            <!--        <div class="item">-->
+            <!--        <img src="/img/advenci.png" style="width:100%;height:300px!important">-->
+            <!--    </div>-->
+            <!--    <div class="item">-->
+            <!--        <img src="/img/addani.png" style="width:100%;height:300px!important">-->
+            <!--    </div>-->
+            <!--</div>-->
+
+            <!-- Left and right controls -->
+            <!--<a class="left carousel-control" href="#myCarousel" data-slide="prev">-->
+            <!--    <span class="glyphicon glyphicon-chevron-left"></span>-->
+            <!--    <span class="sr-only">Previous</span>-->
+            <!--</a>-->
+            <!--<a class="right carousel-control" href="#myCarousel" data-slide="next">-->
+            <!--    <span class="glyphicon glyphicon-chevron-right"></span>-->
+            <!--    <span class="sr-only">Next</span>-->
+            <!--</a>-->
+            <!--</div>-->
+        </div>
+        {{--<div style="width:200px;float:right;">--}}
+        {{--<img src="/img/builders.png" style="width:300px;height:300px">--}}
+        {{--<br>--}}
+        {{--<br>--}}
+        {{--<img src="/img/electrician.png" style="width:300px;height:300px">--}}
         {{--</div>--}}
-    </footer>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="{{ asset('js/custom.js') }}"></script>
+<script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+{{--<script src="/vendor/unisharp/tinymce/js/tinymce/tinymce.min.js"></script>--}}
+{{--<script>tinymce.init({ selector:'textarea' });</script>--}}
+<script>
+
+    CKEDITOR.replace( 'article-ckeditor'  );
+</script>
+</body>
+<footer>
+    {{--<div class="col-xs-6"  style="height:300px">--}}
+    {{--<img src="/img/repair.png" style="width:100%;height:400px">--}}
+    {{--</div>--}}
+
+    {{--<div class="col-xs-6"  style="height:300px">--}}
+    {{--<img src="/img/taxilandscape.jpg" style="width:100%;height:400px">--}}
+    {{--</div>--}}
+</footer>
 </html>
